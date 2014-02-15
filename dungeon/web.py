8032a169fb67a856cb536a7082571b9a3dd57c15
Jason Heeris <jason.heeris@gmail.com>
2014-02-15 22:53:40 +1100

    Fleshed out web interface and added error handling.

--- dungeon/web.py
+++ dungeon/web.py
@@ -18,14 +18,49 @@ dungeon excavator. If not, see <http://www.gnu.org/licenses/>.
 """
 import argparse
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response, redirect, url_for
+
+from dungeon.excavate import render_room
 
 HELP_TEXT = """\
 Web interface to the dungeon excavator."""
 
+app = Flask('dungeon')
+
+@app.route("/")
 def root():
-    """ Web interface page. """
+    """ Web interface landing page. """
     return render_template('index.html')
+
+
+@app.route("/error")
+def error():
+    """ Display errors. """
+    return render_template('error.html')
+
+@app.route("/map.svg", methods=['POST'])
+def process():
+    """ Process submitted form data. """
+    tile_size = int(request.form['size'])
+    wall_file = request.files['walls']
+    floor_file = request.files['floor']
+    floorplan_file = request.files['floorplan']
+    
+    try:
+        room_doc = render_room(
+            floor_file.read(),
+            wall_file.read(),
+            floorplan_file.read(),
+            tile_size
+        )
+    except ValueError:
+        return redirect(url_for('error'))
+        
+    # Create response
+    response = make_response(room_doc)
+    response.headers['Content-Type'] = 'image/svg+xml'
+    return response
+
 
 def main():
     """ Parse arguments and get things going for the web interface """
@@ -39,12 +74,6 @@ def main():
     )
 
     args = parser.parse_args()
-
-    # Web app object
-    app = Flask('dungeon')
-
-    # Front page
-    app.route("/")(root)
 
     app.run(port=args.port, debug=True)
     
